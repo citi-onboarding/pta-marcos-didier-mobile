60c8eaf74efa8d958bcb88e4658124605b244a9b
viniciusgmacedo <macedovinicius2004@gmail.com>
2025-11-20 00:05:56 -0300

    fix:citipet alterated

--- mobile/app/index.tsx
+++ mobile/app/index.tsx
@@ -1,10 +1,10 @@
 import { View, Text } from "react-native";
 import PetCard from "../src/components/MobileCard";
-import { citipet } from "../src/assets";
+import Citipet from "../src/assets/citipet.svg";
 
 const App = () => (
   <View className="flex-1 mt-[91px] items-center bg-gray-100 flex flex-col">
-    <View></View>
+    <Citipet width={160} height={48} />
     <View className="mt-[40px]">
       <Text className="font-sf font-bold text-[24px] ">Sua agenda</Text>
       <Text className="font-sf text-[14px] mt-[12px]">
