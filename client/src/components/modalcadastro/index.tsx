eef3ca7f4c1318d0328b42f14fb12bd3175f1698
viniciusgmacedo <macedovinicius2004@gmail.com>
2025-11-22 04:11:43 -0300

    fix: made functional close button using useState

--- client/src/components/modalcadastro/index.tsx
+++ client/src/components/modalcadastro/index.tsx
@@ -1,9 +1,13 @@
 "use client";
 import { btfechar, citipetlogo } from "@/assets";
 import Image from "next/image";
-import { useForm } from "react-hook-form";
+import { set, useForm } from "react-hook-form";
 
-export default function ModalCadastro() {
+export interface modalprops {
+  abrirModal: (openclose: boolean) => void;
+}
+
+export default function ModalCadastro(props: modalprops) {
   const { register, handleSubmit } = useForm<{ email: string }>();
 
   function onSubmit(data: { email: string }) {
@@ -19,7 +23,12 @@ export default function ModalCadastro() {
             alt="citipetlogo"
             className=" mt-[48px] ml-[54px]"
           />
-          <Image src={btfechar} alt="btfechar" className="ml-[30px]" />
+          <Image
+            src={btfechar}
+            alt="btfechar"
+            className="ml-[30px] cursor-pointer"
+            onClick={() => props.abrirModal(false)}
+          />
         </div>
       </div>
 
