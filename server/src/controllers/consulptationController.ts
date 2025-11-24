7eeaa24b020284563b8a47bae1de34926bbe3393
viniciusgmacedo <macedovinicius2004@gmail.com>
2025-11-24 18:11:06 -0300

    feat

---
+++ server/src/controllers/consulptationController.ts
@@ -0,0 +1,64 @@
+import { Request, Response } from "express";
+import { Citi, Crud } from "../global";
+
+class consulptationController implements Crud {
+  constructor(private readonly citi = new Citi("Consulta")) {}
+
+  create = async (request: Request, response: Response) => {
+    const { medico, descricao, tipo, data, hora, idPaciente } = request.body;
+
+    const isAnyUndefined = this.citi.areValuesUndefined(
+      medico,
+      descricao,
+      tipo,
+      data,
+      hora,
+      idPaciente
+    );
+    if (isAnyUndefined) return response.status(400).send();
+
+    const newConsultation = { medico, descricao, tipo, data, hora, idPaciente };
+    const { httpStatus, message } = await this.citi.insertIntoDatabase(
+      newConsultation
+    );
+
+    return response.status(httpStatus).send({ message });
+  };
+
+  get = async (request: Request, response: Response) => {
+    const { httpStatus, values } = await this.citi.getAll();
+
+    return response.status(httpStatus).send(values);
+  };
+
+  getbyid = async (request: Request, response: Response) => {
+    const { id } = request.params;
+    const { httpStatus, value } = await this.citi.findById(id);
+
+    return response.status(httpStatus).send(value);
+  };
+
+  delete = async (request: Request, response: Response) => {
+    const { id } = request.params;
+
+    const { httpStatus, messageFromDelete } = await this.citi.deleteValue(id);
+
+    return response.status(httpStatus).send({ messageFromDelete });
+  };
+
+  update = async (request: Request, response: Response) => {
+    const { id } = request.params;
+    const { medico, descricao, tipo, data, hora, idPaciente } = request.body;
+
+    const updatedValues = { medico, descricao, tipo, data, hora, idPaciente };
+
+    const { httpStatus, messageFromUpdate } = await this.citi.updateValue(
+      id,
+      updatedValues
+    );
+
+    return response.status(httpStatus).send({ messageFromUpdate });
+  };
+}
+
+export default new consulptationController();
--- server/src/routes.ts
+++ server/src/routes.ts
@@ -1,5 +1,6 @@
 import express from "express";
 import userController from "./controllers/UserController";
+import consulptationController from "./controllers/consulptationController";
 
 const routes = express.Router();
 
@@ -7,5 +8,11 @@ routes.post("/user", userController.create);
 routes.get("/user", userController.get);
 routes.delete("/user/:id", userController.delete);
 routes.patch("/user/:id", userController.update);
+
+routes.post("/consultation", consulptationController.create);
+routes.get("/consultation", consulptationController.get);
+routes.delete("/consultation/:id", consulptationController.delete);
+routes.patch("/consultation/:id", consulptationController.update);
+routes.get("/consultation/:id", consulptationController.getbyid);
 
 export default routes;
